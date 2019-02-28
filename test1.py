e9104ad3240d9a259214db2209341f5536ee5472
Littletittlecatcat <yw.chen.michael@gmail.com>
2019-02-28 15:25:13 +0800

    make it better

--- test1.py
+++ test1.py
@@ -36,13 +36,13 @@ def callback():
     return 'OK'
 
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
-    content = "{}: {}".format(event.source.user_id, event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=content))
+    msg = event.message.text
+    print(msg)
+    msg = msg.encode('utf-8')
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
     
 import os
 if __name__ == "__main__":
