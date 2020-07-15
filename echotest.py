f6c370f529b2d9799cbfd48fc6e5b098dddeeb54
Littletittlecatcat <yw.chen.michael@gmail.com>
2020-07-15 15:40:06 +0800

    version 1

--- echotest.py
+++ echotest.py
@@ -10,15 +10,12 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-
 app = Flask(__name__)
 
-# 填入你的 message api 資訊
-line_bot_api = LineBotApi('Uv9eYbX9E4RxYHBmjwwoUr32eVymZNVkeqva7uvLhh8r8sLVSvUmZuIKx8rmANDE/29D5pGctD5nGeTTylHuHJ6o2DqQt210/+Esz+G/Cp18JCy/E9a5qfjF0DwgscCv5h0RZmzPujiIOBAGGVrQ4AdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('/l3lrV/6VKSQOz0HN5iB/9QQwOJltRAjC+kR3h4g9wdiwGWwt3+i7i069DThcx6r/29D5pGctD5nGeTTylHuHJ6o2DqQt210/+Esz+G/Cp0npnEVH8wqyfWGdD0cOlagY6WIdJy1L+JOcj54WwaXwAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('745051379ec45343551c037a2ba74fc8')
 
 
-# 設定你接收訊息的網址，如 https://YOURAPP.herokuapp.com/callback
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -26,12 +23,13 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    print("Request body: " + body, "Signature: " + signature)
+    app.logger.info("Request body: " + body)
 
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
@@ -44,8 +42,7 @@ def handle_message(event):
         TextSendMessage(text=event.message.text))
 
 
-import os
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=os.environ['PORT'])
+    app.run()
 
 
